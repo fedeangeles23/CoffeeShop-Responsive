306fcf633d572cd1fd5b251f1967f8250f3eb918
FedeAngeles23 <fedeangeles23@gmail.com>
2022-05-31 19:15:03 -0300

    Adding Scripts js folder

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Event menu-button start */
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
